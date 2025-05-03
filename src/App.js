111ebf88bfdafc7eeefee336cc854e277aa9243e
Nobleman4562 <atilolavictor4562@gmail.com>
2025-05-03 15:27:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import Home from "./routes/Home";
+import Rooms from "./routes/Rooms";
+import Explore from "./routes/Explore"
+import { Route, Routes } from "react-router-dom";
+import Facilities from "./routes/Facilities";
+import Contact from "./routes/Contact";
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+        <Routes basename="/Hotel-Website-in-ReactJs">
+          <Route exact path="/Hotel-Website-in-ReactJs" element={<Home/>}/>
+          <Route path="/rooms" element={<Rooms/>}/>
+          <Route path="/explore" element={<Explore/>}/>
+          <Route  path="/facilities" element={<Facilities/>} />
+          <Route  path="/contact" element={<Contact/>} />
+          
+        </Routes>
+        
+    </div>
+  );
+}
